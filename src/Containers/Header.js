69246f4d918de0ac655feb16e125dca404f62b67
Sandip-Pal <sandip.pal@walmartlabs.com>
2019-03-25 13:49:59 +0530

    header redirect to home page

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {browserHistory} from "react-router";
 
 const Header = ()=>{
     return (
         <div className="header">
-          <div>My E-Commerce</div>
+          <div style={{cursor: "pointer"}} onClick={() => browserHistory.push("/")}>My E-Commerce</div>
         </div>
     )
 };
